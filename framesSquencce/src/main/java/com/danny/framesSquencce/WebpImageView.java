2b034b237561d044c1f2f17a09f39933bbb15b25
AndroidDeveloperLB <lblb636@gmail.com>
2018-09-30 15:33:16 +0300

    removed some useless code

--- framesSquencce/src/main/java/com/danny/framesSquencce/WebpImageView.java
+++ framesSquencce/src/main/java/com/danny/framesSquencce/WebpImageView.java
@@ -5,14 +5,13 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.SparseArray;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * this is a ImageView used to display animated WebP files
@@ -21,56 +20,24 @@ import java.util.List;
  */
 
 public class WebpImageView extends android.support.v7.widget.AppCompatImageView {
-    private static final String TAG = "WebpImageView";
-
-    public static final int STATUS_DEFAULT = 0;
-    public static final int STATUS_NEUTRAL = 1;
-    public static final int STATUS_FINAL = 2;
-
+    FrameSequenceDrawable mDrawable;
     // in default, there are only 3 status animations for a WebpImageView
-    private List<FrameSequenceDrawable> drawableList = new ArrayList<>();
+    //private List<FrameSequenceDrawable> drawableList = new ArrayList<>();
 
-    // the default animation count
     private int defaultCount;
-
-    // the neutral animation count
-    private int neutralCount;
-
-    // the final animation count
-    private int finalCount;
 
     private CheckingProvider mProvider = new CheckingProvider();
 
     // stop all webp animation
     public void stop() {
-        if (drawableList.get(STATUS_DEFAULT) != null) {
-            drawableList.get(STATUS_DEFAULT).stop();
-        }
-
-        if (drawableList.get(STATUS_NEUTRAL) != null) {
-            drawableList.get(STATUS_NEUTRAL).stop();
-        }
-
-        if (drawableList.get(STATUS_FINAL) != null) {
-            drawableList.get(STATUS_FINAL).stop();
-        }
+        if (mDrawable != null)
+            mDrawable.stop();
     }
 
     public void destroy() {
-        if (drawableList.get(STATUS_DEFAULT) != null) {
-            drawableList.get(STATUS_DEFAULT).destroy();
-        }
-
-        if (drawableList.get(STATUS_NEUTRAL) != null) {
-            drawableList.get(STATUS_NEUTRAL).destroy();
-        }
-
-        if (drawableList.get(STATUS_FINAL) != null) {
-            drawableList.get(STATUS_FINAL).destroy();
-        }
-
-        drawableList.clear();
-
+        if (mDrawable != null)
+            mDrawable.destroy();
+        mDrawable = null;
         if (mProvider != null) {
             mProvider.empty();
             mProvider = null;
@@ -78,12 +45,12 @@ public class WebpImageView extends android.support.v7.widget.AppCompatImageView 
     }
 
     public static class CheckingProvider implements FrameSequenceDrawable.BitmapProvider {
-        HashSet<Bitmap> mBitmaps = new HashSet<Bitmap>();
+        HashSet<Bitmap> mBitmaps = new HashSet<>();
+
         @Override
         public Bitmap acquireBitmap(int minWidth, int minHeight) {
-            Log.e(TAG, "acquireBitmap here");
-            Bitmap bitmap =
-                    Bitmap.createBitmap(minWidth + STATUS_NEUTRAL, minHeight + 4, Bitmap.Config.ARGB_8888);
+            //Log.e(TAG, "acquireBitmap here");
+            Bitmap bitmap = Bitmap.createBitmap(minWidth, minHeight + 4, Bitmap.Config.ARGB_8888);
             mBitmaps.add(bitmap);
             return bitmap;
         }
@@ -91,7 +58,7 @@ public class WebpImageView extends android.support.v7.widget.AppCompatImageView 
         @Override
         public void releaseBitmap(Bitmap bitmap) {
             if (!mBitmaps.contains(bitmap)) throw new IllegalStateException();
-            Log.e(TAG, "need to releaseBitmap here");
+            //Log.e(TAG, "need to releaseBitmap here");
             mBitmaps.remove(bitmap);
             bitmap.recycle();
         }
@@ -113,14 +80,9 @@ public class WebpImageView extends android.support.v7.widget.AppCompatImageView 
      * Listener which used to get if WebP animation has been finished
      */
     public interface OnWebpFinishListener {
-        /**
-         * a callback to call, pass which WebpIamgeView has been finished animation
-         *
-         * @param status    STATUS_DEFAULT or STATUS_NEUTRAL or STATUS_FINAL
-         */
-        void onAnimationFinished(int status);
+        void onAnimationFinished();
 
-        void onAnimationStart(int status);
+        void onAnimationStart();
     }
 
     public WebpImageView(Context context) {
@@ -131,76 +93,28 @@ public class WebpImageView extends android.support.v7.widget.AppCompatImageView 
         super(context, attrs);
 
         // read all attrs for default & neutral & final status
-        TypedArray mTypedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.webImg);
-
-        defaultCount = mTypedArray.getInteger(R.styleable.webImg_defaultCount, STATUS_NEUTRAL);
-        neutralCount = mTypedArray.getInteger(R.styleable.webImg_neutralCount, STATUS_NEUTRAL);
-        finalCount = mTypedArray.getInteger(R.styleable.webImg_finalCount, STATUS_NEUTRAL);
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.webImg);
+        defaultCount = mTypedArray.getInteger(R.styleable.webImg_defaultCount, 0);
 
         // load raw resources into streams, and get FrameSequenceDrawable, store them in array
-        drawableList.add(initWebpDrawable(mTypedArray.getResourceId(
-                R.styleable.webImg_defaultRawId, -1), defaultCount));
-        if (drawableList.get(STATUS_DEFAULT) != null) {
-            drawableList.get(STATUS_DEFAULT).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
+        mDrawable = initWebpDrawable(mTypedArray.getResourceId(R.styleable.webImg_defaultRawId, -1), defaultCount);
+        if (mDrawable != null) {
+            mDrawable.setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
                 @Override
                 public void onFinished(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationFinished(STATUS_DEFAULT);
-                    }
+                    if (listener != null)
+                        listener.onAnimationFinished();
                 }
 
                 @Override
                 public void onStart(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationStart(STATUS_DEFAULT);
-                    }
+                    if (listener != null)
+                        listener.onAnimationStart();
                 }
             });
         }
-
-        drawableList.add(initWebpDrawable(mTypedArray.getResourceId(
-                R.styleable.webImg_neutralRawId, -STATUS_NEUTRAL), neutralCount));
-        if (drawableList.get(STATUS_NEUTRAL) != null) {
-            drawableList.get(STATUS_NEUTRAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-                @Override
-                public void onFinished(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationFinished(STATUS_NEUTRAL);
-                    }
-                }
-                @Override
-                public void onStart(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationStart(STATUS_NEUTRAL);
-                    }
-                }
-            });
-        }
-
-        drawableList.add(initWebpDrawable(mTypedArray.getResourceId(
-                R.styleable.webImg_finalRawId, -STATUS_NEUTRAL), finalCount));
-        if (drawableList.get(STATUS_FINAL) != null) {
-            drawableList.get(STATUS_FINAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-                @Override
-                public void onFinished(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationFinished(STATUS_FINAL);
-                    }
-                }
-                @Override
-                public void onStart(FrameSequenceDrawable drawable) {
-                    if (listener != null) {
-                        listener.onAnimationStart(STATUS_FINAL);
-                    }
-                }
-            });
-        }
-
         mTypedArray.recycle();
-
-        // default drawable must not be NULL !
-        setImageDrawable(drawableList.get(STATUS_DEFAULT));
+        setImageDrawable(mDrawable);
     }
 
     private FrameSequenceDrawable initWebpDrawable(int resId, int animationCount) {
@@ -246,193 +160,64 @@ public class WebpImageView extends android.support.v7.widget.AppCompatImageView 
     }
 
     public void setDefaultAnimationCount(int defaultAnimationCount) {
-        drawableList.get(STATUS_DEFAULT).setAnimationCount(defaultAnimationCount);
+        mDrawable.setAnimationCount(defaultAnimationCount);
     }
 
-    public void setNeutralAnimationCount(int neutralAnimationCount) {
-        if (drawableList.get(STATUS_NEUTRAL) == null) return;
-        drawableList.get(STATUS_NEUTRAL).setAnimationCount(neutralAnimationCount);
-    }
-
-    public void setFinalAnimationCount(int finalAnimationCount) {
-        if (drawableList.get(STATUS_FINAL) == null) return;
-        drawableList.get(STATUS_FINAL).setAnimationCount(finalAnimationCount);
-    }
-
-    public void setDefaultDrawable(int resId) {
-        if (drawableList.get(STATUS_DEFAULT) != null) {
-            drawableList.get(STATUS_DEFAULT).destroy();
-            drawableList.remove(STATUS_DEFAULT);
-        }
-
-        drawableList.add(STATUS_DEFAULT, initWebpDrawable(resId, 1));
-        drawableList.get(STATUS_DEFAULT).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-            @Override
-            public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_DEFAULT);
-                }
-            }
-            @Override
-            public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_DEFAULT);
-                }
-            }
-        });
-    }
-
-    public void setDefaultDrawable(InputStream is) {
-        if (drawableList.get(STATUS_DEFAULT) != null) {
-            drawableList.get(STATUS_DEFAULT).destroy();
-            drawableList.remove(STATUS_DEFAULT);
-        }
-
-        drawableList.add(STATUS_DEFAULT, initWebpDrawable(is, 1));
-        drawableList.get(STATUS_DEFAULT).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-            @Override
-            public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_DEFAULT);
-                }
-            }
-            @Override
-            public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_DEFAULT);
-                }
-            }
-        });
-    }
 
     public void setNeutralDrawable(int resId) {
-        if (drawableList.get(STATUS_NEUTRAL) != null) {
-            drawableList.get(STATUS_NEUTRAL).destroy();
-            drawableList.remove(STATUS_NEUTRAL);
+        if (mDrawable != null) {
+            mDrawable.destroy();
+            mDrawable = null;
         }
-
-        drawableList.add(STATUS_NEUTRAL, initWebpDrawable(resId, 1));
-        
-        drawableList.get(STATUS_NEUTRAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
+        mDrawable = initWebpDrawable(resId, 1);
+        mDrawable.setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
             @Override
             public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_NEUTRAL);
-                }
+                if (listener != null)
+                    listener.onAnimationFinished();
             }
+
             @Override
             public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_NEUTRAL);
-                }
+                if (listener != null)
+                    listener.onAnimationStart();
             }
         });
     }
 
     public void setNeutralDrawable(InputStream is) {
-        if (drawableList.get(STATUS_NEUTRAL) != null) {
-            drawableList.get(STATUS_NEUTRAL).destroy();
-            drawableList.remove(STATUS_NEUTRAL);
+        if (mDrawable != null) {
+            mDrawable.destroy();
+            mDrawable = null;
         }
-
-        drawableList.add(STATUS_NEUTRAL, initWebpDrawable(is, 1));
-
-        drawableList.get(STATUS_NEUTRAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
+        mDrawable = initWebpDrawable(is, 1);
+        mDrawable.setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
             @Override
             public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_NEUTRAL);
-                }
+                if (listener != null)
+                    listener.onAnimationFinished();
             }
             @Override
             public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_NEUTRAL);
-                }
+                if (listener != null)
+                    listener.onAnimationStart();
             }
         });
     }
 
-    public void setFinalDrawable(int resId) {
-        if (drawableList.get(STATUS_FINAL) != null) {
-            drawableList.get(STATUS_FINAL).destroy();
-            drawableList.remove(STATUS_FINAL);
-        }
-
-        drawableList.add(STATUS_FINAL, initWebpDrawable(resId, 1));
-        
-        drawableList.get(STATUS_FINAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-            @Override
-            public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_FINAL);
-                }
-            }
-            @Override
-            public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_FINAL);
-                }
-            }
-        });
-    }
-
-    public void setFinalDrawable(InputStream is) {
-        if (drawableList.get(STATUS_FINAL) != null) {
-            drawableList.get(STATUS_FINAL).destroy();
-            drawableList.remove(STATUS_FINAL);
-        }
-
-        drawableList.add(STATUS_FINAL, initWebpDrawable(is, 1));
-
-        drawableList.get(STATUS_FINAL).setOnAnimationListener(new FrameSequenceDrawable.OnAnimationListener() {
-            @Override
-            public void onFinished(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationFinished(STATUS_FINAL);
-                }
-            }
-            @Override
-            public void onStart(FrameSequenceDrawable drawable) {
-                if (listener != null) {
-                    listener.onAnimationStart(STATUS_FINAL);
-                }
-            }
-        });
-    }
-
-    // only play default animation
-    public void playAnimation() {
-        playAnimation(STATUS_DEFAULT);
-    }
 
     /**
      * there are 3 status for every Leading Actor in SubMap
-     *
+     * <p>
      * DEFAULT:         loop animation
      * WAIT:            displayed every 7 seconds
      * CELEBRATE:       displayed if the activity completed perfectly
      */
-    public void playAnimation(int status) {
-        switch (status) {
-            case STATUS_DEFAULT:
-                setImageDrawable(drawableList.get(STATUS_DEFAULT));
-
-                drawableList.get(STATUS_DEFAULT).start();
-                break;
-            case STATUS_NEUTRAL:
-                if (drawableList.get(STATUS_NEUTRAL) == null) return;
-                setImageDrawable(drawableList.get(STATUS_NEUTRAL));
-
-                drawableList.get(STATUS_NEUTRAL).start();
-                break;
-            case STATUS_FINAL:
-                if (drawableList.get(STATUS_FINAL) == null) return;
-                setImageDrawable(drawableList.get(STATUS_FINAL));
-
-                drawableList.get(STATUS_FINAL).start();
-                break;
-        }
+    public void playAnimation() {
+        if (mDrawable == null)
+            return;
+        setImageDrawable(mDrawable);
+        mDrawable.start();
     }
 
     public void setFinishedListener(OnWebpFinishListener listener) {
--- sample/src/main/java/com/example/dannyjiang/framesequencedemo/MainActivity.java
+++ sample/src/main/java/com/example/dannyjiang/framesequencedemo/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.dannyjiang.framesequencedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.danny.framesSquencce.WebpImageView;
-
-import static com.danny.framesSquencce.WebpImageView.STATUS_DEFAULT;
-import static com.danny.framesSquencce.WebpImageView.STATUS_FINAL;
-import static com.danny.framesSquencce.WebpImageView.STATUS_NEUTRAL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,68 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        webpImageView = ((WebpImageView) findViewById(R.id.webpImage));
-
+        webpImageView = findViewById(R.id.webpImage);
         // add finish callback
-        webpImageView.setFinishedListener(new WebpImageView.OnWebpFinishListener(){
+        webpImageView.setFinishedListener(new WebpImageView.OnWebpFinishListener() {
 
             @Override
-            public void onAnimationFinished(int status) {
-                switch (status) {
-                    case STATUS_DEFAULT:
-                        Toast.makeText(MainActivity.this, "default webp animation finished",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case STATUS_NEUTRAL:
-                        Toast.makeText(MainActivity.this, "neutral webp animation finished",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case STATUS_FINAL:
-                        Toast.makeText(MainActivity.this, "final webp animation finished",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
+            public void onAnimationFinished() {
+                Toast.makeText(MainActivity.this, "default webp animation finished",
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onAnimationStart(int status) {
-                switch (status) {
-                    case STATUS_DEFAULT:
-                        Toast.makeText(MainActivity.this, "default webp animation start",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case STATUS_NEUTRAL:
-                        Toast.makeText(MainActivity.this, "neutral webp animation start",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case STATUS_FINAL:
-                        Toast.makeText(MainActivity.this, "final webp animation start",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
+            public void onAnimationStart() {
+                Toast.makeText(MainActivity.this, "default webp animation start",
+                        Toast.LENGTH_SHORT).show();
             }
         });
-
-        webpImageView.setDefaultDrawable(getResources().openRawResource(R.raw.capical_g_celebration));
-        webpImageView.setNeutralDrawable(getResources().openRawResource(R.raw.ben_sad_blink_right));
-        webpImageView.setFinalDrawable(getResources().openRawResource(R.raw.ben_happy_talk_right));
-        // set animation count for DEFAULT & NEUTRAL & FINAL animation
+        webpImageView.setNeutralDrawable(getResources().openRawResource(R.raw.ben_happy_talk_right));
         webpImageView.setDefaultAnimationCount(10);
-        webpImageView.setNeutralAnimationCount(10);
-        webpImageView.setFinalAnimationCount(10);
     }
 
     public void defaultAnim(View view) {
-        webpImageView.playAnimation(STATUS_DEFAULT);
-    }
-
-    public void neutralAnim(View view) {
-        webpImageView.playAnimation(WebpImageView.STATUS_NEUTRAL);
-    }
-
-    public void finalAnim(View view) {
-        webpImageView.playAnimation(WebpImageView.STATUS_FINAL);
+        webpImageView.playAnimation();
     }
 
     @Override
